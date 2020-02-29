b7e9d630494ef98dd053c645632c25620bab35cc
Anca Simon <ancarsimon@gmail.com>
2020-02-29 10:33:50 -0600

    added sharks to tank

--- javascripts/components/tank.js
+++ javascripts/components/tank.js
@@ -1,17 +1,35 @@
 import utils from '../helpers/utils.js';
+import personData from '../helpers/data/personData.js';
 
+// USING a regular for loop:
+// const tankBuilder = (list) => {
+//     const list = personData.getAlivePersons();
+//     let domString = '';
+//     domString += '<div class="card" style="width: 18rem;">';
+//     domString += `<div class="card-header">Shark Tank</div>`;
+//     domString += '<ul class="list-group list-group-flush">';
+//     for (let i=0; i < list.length; i++) {
+//         domString += `<li class="list-group-item">${list[i].name}</li>`;
+//     }
+//     domString += '</ul>';
+//     domString += '</div>';
+
+//     utils.printToDom('tank', domString);
+// };
+
+// OR Using a forEach loop (and in that case, you don't need a list parameter in the function):
 const tankBuilder = () => {
-    let domString = 'help2';
-    <div class="card" style="width: 18rem;">
-  <div class="card-header">
-    Featured
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
-</div>
+    const persons = personData.getAlivePersons();
+    let domString = '';
+    domString += '<div class="card">';
+    domString += `<div class="card-header">Shark Tank</div>`;
+    domString += '<ul class="list-group list-group-flush">';
+    persons.forEach((person) => {
+        domString += `<li class="list-group-item">${person.name}</li>`;
+    })
+    domString += '</ul>';
+    domString += '</div>';
+
     utils.printToDom('tank', domString);
 };
 
