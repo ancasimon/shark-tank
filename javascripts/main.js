7b9efb25be0d1c0c1db153ff10344bc4b34d0fc4
Anca Simon <ancarsimon@gmail.com>
2020-02-29 11:35:00 -0600

    added shark attach button

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,19 @@
 import tank from './components/tank.js';
 import graveyard from './components/graveyard.js';
+import personData from './helpers/data/personData.js';
+
+const sharkAttackEvent = () => {
+    console.log('ouch');
+    //randomly pick someone in personDatA
+    personData.randomMurder();
+    tank.tankBuilder();
+    graveyard.buildGraveyard();
+};
 
 const init = () => {
     tank.tankBuilder();
     graveyard.buildGraveyard();
+    $('#bite-me').click(sharkAttackEvent);
 };
 
 init();
